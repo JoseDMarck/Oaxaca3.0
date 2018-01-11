942001131f2234baba47ca7d86bb5779aeb5806c
Deuteros <josewbmx@gmail.com>
2018-01-11 01:10:39 -0600

    se modifico la url

--- compilacion-code/compilacion.php
+++ compilacion-code/compilacion.php
@@ -7,4 +7,7 @@ ng build --prod --deploy-url="/2018elecciones.com/wp-content/themes/wpng2/dist/"
 
 ng build --prod --deploy-url="/demos/AudiosCDO/wp-content/themes/wpng2/dist/"
 
+
+ng build --prod --deploy-url="/wp-content/themes/wpng2/dist/"
+
  
