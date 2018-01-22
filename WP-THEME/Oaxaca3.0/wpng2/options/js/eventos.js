f4ad68aa7167d7411f68deb494b035da12908bb3
Deuteros <josewbmx@gmail.com>
2018-01-22 17:36:23 -0600

    se agregaron los js para los eventos de subir banners

---
+++ WP-THEME/Oaxaca3.0/wpng2/options/js/eventos.js
@@ -0,0 +1,77 @@
+ 
+
+ 
+$(".closebtnPopup, .button-aceptar").click(function(){
+	$(".Bloque_popup").fadeOut();
+	$(".ContenedorBanner_1").hide();		 
+	$(".ContenedorBanner_2").hide();		 
+	$(".ContenedorBanner_3").hide();		 
+	$(".ContenedorBanner_4").hide();		 
+	$(".ContenedorBanner_5").hide();		 
+	$(".ContenedorBanner_6").hide();		 
+	$(".ContenedorBanner_7").hide();		 
+	$(".ContenedorBanner_8").hide();		 
+	$(".ContenedorBanner_9").hide();		 
+	$(".ContenedorBanner_10").hide();		 
+	$(".ContenedorBanner_11").hide();		 
+	$(".ContenedorBanner_12").hide();		 
+	$(".ContenedorBanner_13").hide();		 
+	$(".ContenedorBanner_14").hide();		 
+	$(".ContenedorBanner_15").hide();		 
+});
+
+
+
+$(".openBanner_1").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_1").fadeIn();	
+});
+
+
+$(".openBanner_2").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_2").fadeIn();	
+});
+
+$(".openBanner_3").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_3").fadeIn();	
+});
+
+$(".openBanner_4").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_4").fadeIn();	
+});
+
+$(".openBanner_5").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_5").fadeIn();	
+});
+
+$(".openBanner_6").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_6").fadeIn();	
+});
+
+$(".openBanner_7").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_7").fadeIn();	
+});
+
+$(".openBanner_8").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_8").fadeIn();	
+});
+
+$(".openBanner_9").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_9").fadeIn();	
+});
+
+
+$(".openBanner_10").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_10").fadeIn();	
+});
+
+ 
