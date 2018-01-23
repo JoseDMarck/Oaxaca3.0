471ca811436755c26e004d8d728d9a91926f2ac0
Jose Deuteros <josewbmx@gmail.com>
2018-01-23 00:19:43 -0600

    se quito un metabox que no se ocupaba

--- WP-THEME/Oaxaca3.0/wpng2/options/banners/REST_API_REGISTER/generales.php
+++ WP-THEME/Oaxaca3.0/wpng2/options/banners/REST_API_REGISTER/generales.php
@@ -1,29 +1,6 @@
 <?php
 
-/*------------------------------------------*\
-     BANNERS PAGE RESISTER ON REST API
-\*-------------------------------------------*/
-add_action( 'rest_api_init', 'slug_register_id_imagen_prueba' );
-function slug_register_id_imagen_prueba() {
-    register_rest_field( 'page',
-        'imagen_prueba',
-        array(
-            'get_callback'    => 'slug_get_imagen_prueba',
-            'update_callback' => 'slug_update_imagen_prueba',
-            'schema'          => null,
-        )
-    );
-}
-
-function slug_get_imagen_prueba( $post, $field_name, $request ) {
-     return get_post_meta($post['id'], 'id_imagen_prueba', true);
-}
-
-
-function slug_update_imagen_prueba( $value, $object, $field_name ) {
-    if ( ! $value || ! is_string( $value ) ) { return; }
-    return update_post_meta( $object->ID, $field_name, strip_tags( $value ) );
-}
+ 
 
 /*------------------------------------------*\
      BANNERS PAGE RESISTER ON REST API
