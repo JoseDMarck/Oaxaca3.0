6d4a2dcaa45062f77f7bde8e0d3fa0553c634ee0
Jose Deuteros <josewbmx@gmail.com>
2018-01-23 04:10:19 -0600

    Se agregaron los componentes de banners para post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,31 +17,35 @@ import { InternetComponent  } from './posts/internet/internet.component';
 import { DeportesComponent  } from './posts/deportes/deportes.component';
 import { EspectaculosComponent  } from './posts/espectaculos/espectaculos.component';
 import { VideosComponent  } from './posts/videos/videos.component';
-import { OaxacaComponent  } from './posts/oaxaca/oaxaca.component'
+import { OaxacaComponent  } from './posts/oaxaca/oaxaca.component';
 
 //BANNERS POST
-import {  BannerPostTopComponent } from './banners/banner-post-top/banner-post-top.component'
-import {  BannerPostBottomComponent } from './banners/banner-post-bottom/banner-post-bottom.component'
+import {  BannerPost1Component } from './banners/banner-post-1/banner-post-1.component';
+import {  BannerPost2Component } from './banners/banner-post-2/banner-post-2.component';
+import {  BannerPost3Component } from './banners/banner-post-3/banner-post-3.component';
+import {  BannerPost4Component } from './banners/banner-post-4/banner-post-4.component';
+import {  BannerPost5Component } from './banners/banner-post-5/banner-post-5.component';
+
 
 
 //BANNERS HOME
-import {  BannerHeaderHomeComponent } from './banners/banner-header-home/banner-header-home.component'
-import {  BannerHome1Component  } from './banners/banner-home-1/banner-home-1.component'
-import {  BannerHome2Component  } from './banners/banner-home-2/banner-home-2.component'
-import {  BannerHome3Component  } from './banners/banner-home-3/banner-home-3.component'
-import {  BannerHome4Component  } from './banners/banner-home-4/banner-home-4.component'
-import {  BannerHome5Component  } from './banners/banner-home-5/banner-home-5.component'
-import {  BannerHome6Component  } from './banners/banner-home-6/banner-home-6.component'
-import {  BannerHome7Component  } from './banners/banner-home-7/banner-home-7.component'
-import {  BannerHome8Component  } from './banners/banner-home-8/banner-home-8.component'
-import {  BannerHome9Component  } from './banners/banner-home-9/banner-home-9.component'
-import {  BannerHome10Component  } from './banners/banner-home-10/banner-home-10.component'
+import {  BannerHeaderHomeComponent } from './banners/banner-header-home/banner-header-home.component';
+import {  BannerHome1Component  } from './banners/banner-home-1/banner-home-1.component';
+import {  BannerHome2Component  } from './banners/banner-home-2/banner-home-2.component';
+import {  BannerHome3Component  } from './banners/banner-home-3/banner-home-3.component';
+import {  BannerHome4Component  } from './banners/banner-home-4/banner-home-4.component';
+import {  BannerHome5Component  } from './banners/banner-home-5/banner-home-5.component';
+import {  BannerHome6Component  } from './banners/banner-home-6/banner-home-6.component';
+import {  BannerHome7Component  } from './banners/banner-home-7/banner-home-7.component';
+import {  BannerHome8Component  } from './banners/banner-home-8/banner-home-8.component';
+import {  BannerHome9Component  } from './banners/banner-home-9/banner-home-9.component';
+import {  BannerHome10Component  } from './banners/banner-home-10/banner-home-10.component';
 
 
 
 
 
-import {ShareModule} from 'ng2share/share.module'
+import {ShareModule} from 'ng2share/share.module';
 import { MenuGeneralComponent } from './posts/menu-general/menu-general.component';
 import { BusquedaComponent } from './posts/busqueda/busqueda.component';
 import { RouterModule } from '@angular/router';
@@ -70,8 +74,12 @@ import { AdDFPComponent } from 'ng2-ad-dfp';
     OaxacaComponent,
 
     //Banners 
-    BannerPostTopComponent,
-    BannerPostBottomComponent,
+    BannerPost1Component,
+    BannerPost2Component,
+    BannerPost3Component,
+    BannerPost4Component,
+    BannerPost5Component,
+
 
     //Banners Home
     BannerHeaderHomeComponent,
