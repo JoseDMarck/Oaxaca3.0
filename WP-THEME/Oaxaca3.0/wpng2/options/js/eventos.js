f383afc5e47d58a63daa4f4e95920cc102d0a478
Jose Deuteros <josewbmx@gmail.com>
2018-01-23 00:20:00 -0600

    Se agregaron los eventos para el funcionamiento de los banners

--- WP-THEME/Oaxaca3.0/wpng2/options/js/eventos.js
+++ WP-THEME/Oaxaca3.0/wpng2/options/js/eventos.js
@@ -3,6 +3,7 @@
  
 $(".closebtnPopup, .button-aceptar").click(function(){
 	$(".Bloque_popup").fadeOut();
+	$(".Bloque_popupPost").fadeOut();
 	$(".ContenedorBanner_1").hide();		 
 	$(".ContenedorBanner_2").hide();		 
 	$(".ContenedorBanner_3").hide();		 
@@ -13,11 +14,15 @@ $(".closebtnPopup, .button-aceptar").click(function(){
 	$(".ContenedorBanner_8").hide();		 
 	$(".ContenedorBanner_9").hide();		 
 	$(".ContenedorBanner_10").hide();		 
-	$(".ContenedorBanner_11").hide();		 
-	$(".ContenedorBanner_12").hide();		 
-	$(".ContenedorBanner_13").hide();		 
-	$(".ContenedorBanner_14").hide();		 
-	$(".ContenedorBanner_15").hide();		 
+	$(".ContenedorBanner_11").hide();	
+
+
+	$(".ContenedorBannerPost_1").hide();	
+	$(".ContenedorBannerPost_2").hide();	
+	$(".ContenedorBannerPost_3").hide();	
+	$(".ContenedorBannerPost_4").hide();	
+	$(".ContenedorBannerPost_5").hide();	
+
 });
 
 
@@ -73,5 +78,59 @@ $(".openBanner_10").click(function(){
 	$(".Bloque_popup").fadeIn();
 	$(".ContenedorBanner_10").fadeIn();	
 });
+
+
+$(".openBanner_11").click(function(){
+	$(".Bloque_popup").fadeIn();
+	$(".ContenedorBanner_11").fadeIn();	
+});
+
+
+
+//BANNERS PARA POST ==============================
+$(".openBannerPost_1").click(function(){
+	$(".Bloque_popupPost").fadeIn();
+	$(".ContenedorBannerPost_1").fadeIn();	
+});
+
+$(".openBannerPost_2").click(function(){
+	$(".Bloque_popupPost").fadeIn();
+	$(".ContenedorBannerPost_2").fadeIn();	
+});
+
+
+$(".openBannerPost_3").click(function(){
+	$(".Bloque_popupPost").fadeIn();
+	$(".ContenedorBannerPost_3").fadeIn();	
+});
+
+$(".openBannerPost_4").click(function(){
+	$(".Bloque_popupPost").fadeIn();
+	$(".ContenedorBannerPost_4").fadeIn();	
+});
+
+$(".openBannerPost_5").click(function(){
+	$(".Bloque_popupPost").fadeIn();
+	$(".ContenedorBannerPost_5").fadeIn();	
+});
+
+
+//$("#category").on("change", toggleFields);
+
+$( "#tipoBanner").change(function() {
+  	if ($("#tipoBanner").val() == "opcion_1") {
+  		$(".BloqueMain").fadeIn();
+  		$(".BloqueMainPost").hide();
+  	}
+
+  	if ($("#tipoBanner").val() == "opcion_2") {
+  		$(".BloqueMainPost").fadeIn();	
+  		$(".BloqueMain").hide();
+
+  	}
+});
+
+
+ 
 
  
