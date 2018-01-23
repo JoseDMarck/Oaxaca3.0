56763413fe68fabe4f2d5b7d328c22651bb7e6b9
Jose Deuteros <josewbmx@gmail.com>
2018-01-23 00:18:59 -0600

    Se agregaron los metabox para los banners restantes

--- WP-THEME/Oaxaca3.0/wpng2/options/banners/metaboxes.php
+++ WP-THEME/Oaxaca3.0/wpng2/options/banners/metaboxes.php
@@ -6,7 +6,7 @@
   function page_banners( $post ) {
   add_meta_box( 
   'em-img-meta', // atributo ID
-  '<h1 style="color:#2980b9">Banners para el home</h1>', // Título
+  '<h1 style="color:#2980b9">Banners de publicidad </h1>', // Título
   'banners_home', // Función que muestra el HTML que aparecerá en la pantalla
   'page', // Tipo de entrada. Puede ser 'post', 'page', 'link', o 'custom_post_type'
   'normal', // Parte de la pantalla donde aparecerá. Puede ser 'normal', 'advanced', o 'side'
@@ -16,9 +16,7 @@
 
   function banners_home( $post ) {
   
-  $value_id_imagen_prueba = (get_post_meta($post->ID, 'id_imagen_prueba', true));
-  $value_id_imagen_page = (get_post_meta($post->ID, 'id_imagen_page', true));
-
+ 
   //BANNER 1
   $analitycs_banner_home_1 = (get_post_meta($post->ID, 'analitycs_banner_home_1', true));
   $url_banner_home_1 = (get_post_meta($post->ID, 'url_banner_home_1', true));
@@ -34,6 +32,77 @@
   $url_banner_home_3 = (get_post_meta($post->ID, 'url_banner_home_3', true));
   $imagen_banner_3 = (get_post_meta($post->ID, 'imagen_banner_3', true)); 
 
+   //BANNER _4
+  $analitycs_banner_home_4 = (get_post_meta($post->ID, 'analitycs_banner_home_4', true));
+  $url_banner_home_4 = (get_post_meta($post->ID, 'url_banner_home_4', true));
+  $imagen_banner_4 = (get_post_meta($post->ID, 'imagen_banner_4', true)); 
+
+ //BANNER _5
+  $analitycs_banner_home_5 = (get_post_meta($post->ID, 'analitycs_banner_home_5', true));
+  $url_banner_home_5 = (get_post_meta($post->ID, 'url_banner_home_5', true));
+  $imagen_banner_5 = (get_post_meta($post->ID, 'imagen_banner_5', true)); 
+
+
+ //BANNER _6
+  $analitycs_banner_home_6 = (get_post_meta($post->ID, 'analitycs_banner_home_6', true));
+  $url_banner_home_6 = (get_post_meta($post->ID, 'url_banner_home_6', true));
+  $imagen_banner_6 = (get_post_meta($post->ID, 'imagen_banner_6', true)); 
+
+   //BANNER _7
+  $analitycs_banner_home_7 = (get_post_meta($post->ID, 'analitycs_banner_home_7', true));
+  $url_banner_home_7 = (get_post_meta($post->ID, 'url_banner_home_7', true));
+  $imagen_banner_7 = (get_post_meta($post->ID, 'imagen_banner_7', true)); 
+
+
+   //BANNER _8
+  $analitycs_banner_home_8 = (get_post_meta($post->ID, 'analitycs_banner_home_8', true));
+  $url_banner_home_8 = (get_post_meta($post->ID, 'url_banner_home_8', true));
+  $imagen_banner_8 = (get_post_meta($post->ID, 'imagen_banner_8', true)); 
+
+
+   //BANNER _9
+  $analitycs_banner_home_9 = (get_post_meta($post->ID, 'analitycs_banner_home_9', true));
+  $url_banner_home_9 = (get_post_meta($post->ID, 'url_banner_home_9', true));
+  $imagen_banner_9 = (get_post_meta($post->ID, 'imagen_banner_9', true)); 
+
+   //BANNER _10
+  $analitycs_banner_home_10 = (get_post_meta($post->ID, 'analitycs_banner_home_10', true));
+  $url_banner_home_10 = (get_post_meta($post->ID, 'url_banner_home_10', true));
+  $imagen_banner_10 = (get_post_meta($post->ID, 'imagen_banner_10', true));
+
+   //BANNER _11
+  $analitycs_banner_home_11 = (get_post_meta($post->ID, 'analitycs_banner_home_11', true));
+  $url_banner_home_11 = (get_post_meta($post->ID, 'url_banner_home_11', true));
+  $imagen_banner_11 = (get_post_meta($post->ID, 'imagen_banner_11', true));
+
+
+
+  //BANNER POST ====================
+
+  //BANNER POST _1
+  $analitycs_banner_post_1 = (get_post_meta($post->ID, 'analitycs_banner_post_1', true));
+  $url_banner_post_1 = (get_post_meta($post->ID, 'url_banner_post_1', true));
+  $imagen_banner_post_1= (get_post_meta($post->ID, 'imagen_banner_post_1', true));
+
+  //BANNER POST _2
+  $analitycs_banner_post_2 = (get_post_meta($post->ID, 'analitycs_banner_post_2', true));
+  $url_banner_post_2 = (get_post_meta($post->ID, 'url_banner_post_2', true));
+  $imagen_banner_post_2= (get_post_meta($post->ID, 'imagen_banner_post_2', true)); 
+
+   //BANNER POST _3
+  $analitycs_banner_post_3 = (get_post_meta($post->ID, 'analitycs_banner_post_3', true));
+  $url_banner_post_3 = (get_post_meta($post->ID, 'url_banner_post_3', true));
+  $imagen_banner_post_3= (get_post_meta($post->ID, 'imagen_banner_post_3', true)); 
+
+  //BANNER POST _4
+  $analitycs_banner_post_4 = (get_post_meta($post->ID, 'analitycs_banner_post_4', true));
+  $url_banner_post_4 = (get_post_meta($post->ID, 'url_banner_post_4', true));
+  $imagen_banner_post_4= (get_post_meta($post->ID, 'imagen_banner_post_4', true)); 
+
+  //BANNER POST _5
+  $analitycs_banner_post_5 = (get_post_meta($post->ID, 'analitycs_banner_post_5', true));
+  $url_banner_post_5 = (get_post_meta($post->ID, 'url_banner_post_5', true));
+  $imagen_banner_post_5= (get_post_meta($post->ID, 'imagen_banner_post_5', true)); 
 
 ?>
 
@@ -87,11 +156,20 @@
 
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/options/styleBanners.css">
 
-<b><span style="color:#1abc9c">imagen prueba:</span></b> <br><br>
-<input type="text" name="id_imagen_prueba" id="id_imagen_prueba" value="<?php echo $value_id_imagen_prueba; ?>" style="width: 600px;" /><br><br>
-
+ 
+<div style="display: none;">
 <b><span style="color:#1abc9c">Mes titulo:</span></b> <br><br>
-<input type="text" name="id_imagen_page" id="id_imagen_page" value="<?php echo $value_id_imagen_page; ?>" style="width: 600px;" /><br><br>
+<input type="text" name="id_imagen_page" id="id_imagen_page" value="<?php echo $value_id_imagen_page; ?>" style="width: 600px;" /><br><br><br><br><br><br>
+</div>
+
+
+
+<h1>Escogé el tipo de banner que deseas agregar</h1><br>
+<select id="tipoBanner" name="tipoBanner">
+  <option value="opcion_1" selected="selected">Banners para el home</option>
+  <option value="opcion_2">Banners para post</option><br><br>
+</select>
+
 
 <div class="BloqueMain">
 
@@ -111,19 +189,57 @@
 
 <div class="Bloque_popup">
 
-∑
 
 <?php 
 // METABOX CARTELERA
 include 'banners_home/banner_1.php';
 include 'banners_home/banner_2.php';
 include 'banners_home/banner_3.php';
+include 'banners_home/banner_4.php';
+include 'banners_home/banner_5.php';
+include 'banners_home/banner_6.php';
+include 'banners_home/banner_7.php';
+include 'banners_home/banner_8.php';
+include 'banners_home/banner_9.php';
+include 'banners_home/banner_10.php';
+include 'banners_home/banner_11.php';
+
+
 ?>
 
-
-
 </div> <!-- Bloque_poup -->
+
+
 </div><!-- Bloque Main -->
+
+
+<!-- Banners para post ============= -->
+
+<div class="BloqueMainPost">
+
+<div class="Bloque_post_1 skake openBannerPost_1"><div class="txt_banners">Banner 1</div></div>
+<div class="Bloque_post_2 skake openBannerPost_2"><div class="txt_banners">Banner 2</div></div>
+<div class="Bloque_post_3 skake openBannerPost_3"><div class="txt_banners">Banner 3</div></div>
+<div class="Bloque_post_4 skake openBannerPost_4"><div class="txt_banners">Banner 4</div></div>
+<div class="Bloque_post_5 skake openBannerPost_5"><div class="txt_banners">Banner 5</div></div>
+
+</div><!-- BloqueMainPost -->
+
+
+<div class="Bloque_popupPost">
+
+<?php 
+//Banners para post
+include 'banners_post/banner_1.php';
+include 'banners_post/banner_2.php';
+include 'banners_post/banner_3.php';
+include 'banners_post/banner_4.php';
+include 'banners_post/banner_5.php';
+?>
+
+</div>
+
+
 
 <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/options/js/jquery.1.9.1.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/options/js/eventos.js"></script>
@@ -144,6 +260,23 @@ include 'REST_API_REGISTER/generales.php';
 include 'REST_API_REGISTER/banner_1.php';
 include 'REST_API_REGISTER/banner_2.php';
 include 'REST_API_REGISTER/banner_3.php';
+include 'REST_API_REGISTER/banner_4.php';
+include 'REST_API_REGISTER/banner_5.php';
+include 'REST_API_REGISTER/banner_6.php';
+include 'REST_API_REGISTER/banner_7.php';
+include 'REST_API_REGISTER/banner_8.php';
+include 'REST_API_REGISTER/banner_9.php';
+include 'REST_API_REGISTER/banner_10.php';
+include 'REST_API_REGISTER/banner_11.php';
+
+
+// REST API BANNERS  POSAT
+include 'REST_API_REGISTER/banner_post_1.php';
+include 'REST_API_REGISTER/banner_post_2.php';
+include 'REST_API_REGISTER/banner_post_3.php';
+include 'REST_API_REGISTER/banner_post_4.php';
+include 'REST_API_REGISTER/banner_post_5.php';
+
 
 ?>
 
